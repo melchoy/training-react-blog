829c38bdb582246cb9275957422d126ae04ba3dd
Melissa Choy <mel@melissachoy.com>
2019-11-20 07:26:48 +1100

    reducers and dispatching correct values

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,10 +8,15 @@ class PostList extends React.Component {
   }
 
   render() {
+    console.log(this.props);
     return(
       <div>Post List</div>
     );
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
